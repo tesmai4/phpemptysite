4353ec4dfb47a728a74519a7af06b153c59fccfc
Tariq <tesmai4@users.noreply.github.com>
2016-11-28 16:51:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,33 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
-<?php
-
-require(__DIR__.'/init.php');
-htmlHeader();
-
-// check solarium version available
-echo 'Solarium library version: ' . Solarium\Client::VERSION . ' - ';
-
-// create a client instance
-$client = new Solarium\Client($config);
-
-// create a ping query
-$ping = $client->createPing();
-
-// execute the ping query
-try {
-    $result = $client->ping($ping);
-    echo 'Ping query successful';
-    echo '<br/><pre>';
-    var_dump($result->getData());
-    echo '</pre>';
-} catch (Solarium\Exception $e) {
-    echo 'Ping query failed';
-}
-
-htmlFooter();
-
-?>
-</body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
 </html>
